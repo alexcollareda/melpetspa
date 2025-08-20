1eba4283536e23dbd8e86d083c2a44ea55869b07
Collaredaa <gustavo.collareda.oliveira@gmail.com>
2025-08-20 02:49:24 -0300

    KAN-8 Ajustando o back para a filtragem de checkins

--- src/main/java/br/com/melpetspa/melpetspa/repository/ServiceRepository.java
+++ src/main/java/br/com/melpetspa/melpetspa/repository/ServiceRepository.java
@@ -1,11 +1,8 @@
 package br.com.melpetspa.melpetspa.repository;
 
 import br.com.melpetspa.melpetspa.entity.ServiceEntity;
-import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Integer> {
